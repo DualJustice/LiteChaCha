00258e179334ca815c24ba6fe2b6ad91f8b0c15e
DualJustice <mattatgbsoftware@gmail.com>
2021-06-01 16:00:00 -0500

    Scuffed work
    Got a somewhat functioning implementation of ladderStep. Also toyed with the idea of removing the use of the Big Numbers library.

--- tests/TimeTests/src/include/X25519V2.h
+++ tests/TimeTests/src/include/X25519V2.h
@@ -1,12 +1,10 @@
-#ifndef X25519_H
-#define X25519_H
+#ifndef X25519V2_H
+#define X25519V2_H
 
 #include "Arduino.h" // DELETE ME!
 #include "HardwareSerial.h" // DELETE ME!
 
-//#include <stdint.h>
-
-#include "BigNumber.h"
+#include <stdint.h>
 
 
 //https://datatracker.ietf.org/doc/html/rfc7748
@@ -16,36 +14,35 @@
 //https://tches.iacr.org/index.php/TCHES/article/view/8681/8240 <-------------------------------------------------- I don't believe this is applicable. I think curve25519 is a Montgomery curve, not a Weierstrass curve.
 //https://eprint.iacr.org/eprint-bin/getfile.pl?entry=2019/1410&version=20191205:080951&file=1410.pdf <---------------------- This is maybe worth looking into.
 //https://www.researchgate.net/publication/277940984_High-speed_Curve25519_on_8-bit_16-bit_and_32-bit_microcontrollers
-//https://www.gammon.com.au/forum/?id=11519
 
 
 class X25519KeyManagement {
 private:
-	BigNumber T1;
-	BigNumber T2;
-	BigNumber T3;
-	BigNumber T4;
-	BigNumber T5;
-	BigNumber T6;
-	BigNumber X1;
-	BigNumber X3;
-	BigNumber Z3;
-	BigNumber X2;
-	BigNumber Z2;
+	T1;
+	T2;
+	T3;
+	T4;
+	T5;
+	T6;
+	X1;
+	X3;
+	Z3;
+	X2;
+	Z2;
 
 	char bit;
 
-//	BigNumber U[4] = {X2, Z2, X3, Z3};
-//	BigNumber V[4] = {X3, Z3, X2, Z2};
+//	U[4] = {X2, Z2, X3, Z3};
+//	V[4] = {X3, Z3, X2, Z2};
 
 	static const constexpr unsigned long A24 = 121665; // (486662 - 2)/4
-	BigNumber p = "57896044618658097711785492504343953926634992332820282019728792003956564819949"; // (2^255) - 19
+	p = "57896044618658097711785492504343953926634992332820282019728792003956564819949"; // (2^255) - 19
 
 	char* bytesLittleEndian(char*, unsigned short);
 	char* transformScalar(char*);
-	BigNumber makeInt(char*, unsigned short);
-	void ladderStep(BigNumber, BigNumber&, BigNumber&, BigNumber&, BigNumber&);
-	BigNumber curve25519(BigNumber, char*, BigNumber);
+	makeInt(char*, unsigned short);
+	void ladderStep(, *);
+	curve25519(, char*, );
 public:
 	X25519KeyManagement();
 	~X25519KeyManagement();
@@ -67,7 +64,7 @@ X25519KeyManagement::~X25519KeyManagement() {
 
 
 void X25519KeyManagement::startBigNum() {
-	BigNumber::begin();
+	::begin();
 }
 
 
@@ -97,8 +94,8 @@ char* X25519KeyManagement::transformScalar(char* n) {
 }
 
 
-BigNumber X25519KeyManagement::makeInt(char* b, unsigned short c) {
-	BigNumber bigInt; // Potential memory leak?
+X25519KeyManagement::makeInt(char* b, unsigned short c) {
+	bigInt; // Potential memory leak?
 
 	for(unsigned short i = 0; i < c; i += 1) {
 		bigInt += (b[i])*(256^i);
@@ -108,71 +105,35 @@ BigNumber X25519KeyManagement::makeInt(char* b, unsigned short c) {
 }
 
 
-void X25519KeyManagement::ladderStep(BigNumber X1, BigNumber& UV0, BigNumber& UV1, BigNumber& UV2, BigNumber& UV3) {
+void X25519KeyManagement::ladderStep(X1, * UV) {
 /*
 X2 = UV[0]
 Z2 = UV[1]
 X3 = UV[2]
 Z3 = UV[3]
 */
-	T1 = UV0 + UV1;			// 1
-//	Serial.print("T1: ");
-//	Serial.println(T1);
-	T2 = UV0 - UV1;			// 2
-//	Serial.print("T2: ");
-//	Serial.println(T2);
-	T3 = UV2 + UV3;			// 3
-//	Serial.print("T3: ");
-//	Serial.println(T3);
-	T4 = UV2 - UV3;			// 4
-//	Serial.print("T4: ");
-//	Serial.println(T4);
-	T5 = T1.pow(2);			// 5
-//	Serial.print("T5: ");
-//	Serial.println(T5);
-	T6 = T2.pow(2);			// 6
-//	Serial.print("T6: ");
-//	Serial.println(T6);
-	T2 *= T3;				// 7
-//	Serial.print("T2: ");
-//	Serial.println(T2);
-	T1 *= T4;				// 8
-//	Serial.print("T1: ");
-//	Serial.println(T1);
-	T1 += T2;				// 9
-//	Serial.print("T1: ");
-//	Serial.println(T1);
-	T2 = T1 - T2;			// 10
-//	Serial.print("T2: ");
-//	Serial.println(T2);
-	UV2 = (T1.pow(2)) % p;	// 11
-//	Serial.print("UV2: ");
-//	Serial.println(UV2);
-	T2 = T2.pow(2);			// 12
-//	Serial.print("T2: ");
-//	Serial.println(T2);
-	UV3 = (T2*X1) % p;		// 13
-//	Serial.print("UV3: ");
-//	Serial.println(UV3);
-	UV0 = (T5*T6) % p;		// 14
-//	Serial.print("UV0: ");
-//	Serial.println(UV0);
-	T5 -= T6;				// 15
-//	Serial.print("T5: ");
-//	Serial.println(T5);
-	T1 = A24*T5;			// 16
-//	Serial.print("T1: ");
-//	Serial.println(T1);
-	T6 += T1;				// 17
-//	Serial.print("T6: ");
-//	Serial.println(T6);
-	UV1 = (T5*T6) % p;		// 18
-//	Serial.print("UV1: ");
-//	Serial.println(UV1);
+	T1 = UV[0] + UV[1];	// 1
+	T2 = UV[0] - UV[1];	// 2
+	T3 = UV[2] + UV[3];	// 3
+	T4 = UV[2] - UV[3];	// 4
+	T5 = T1^2;			// 5
+	T6 = T2^2;			// 6
+	T2 *= T3;			// 7
+	T1 *= T4;			// 8
+	T1 += T2;			// 9
+	T2 = T1 - T2;		// 10
+	UV[2] = T1^2;		// 11
+	T2 = T2^2;			// 12
+	UV[3] = T2*X1;		// 13
+	UV[0] = T5*T6;		// 14
+	T5 -= T6;			// 15
+	T1 = A24*T5;		// 16
+	T6 += T1;			// 17
+	UV[1] = T5*T6;		// 18
 }
 
 
-BigNumber X25519KeyManagement::curve25519(BigNumber nInt, char* n, BigNumber xInit) {
+X25519KeyManagement::curve25519(nInt, char* n, xInit) {
 	X1 = xInit;
 	X2 = 1;
 	Z2 = 0;
@@ -186,8 +147,8 @@ BigNumber X25519KeyManagement::curve25519(BigNumber nInt, char* n, BigNumber xIn
 	Serial.println(Z3);
 	Serial.println();
 */
-//BigNumber U[4] = {X2, Z2, X3, Z3};
-//BigNumber V[4] = {X3, Z3, X2, Z2};
+U[4] = {X2, Z2, X3, Z3};
+V[4] = {X3, Z3, X2, Z2};
 /*
 	Serial.println(U[0]);
 	Serial.println(U[1]);
@@ -205,33 +166,29 @@ BigNumber X25519KeyManagement::curve25519(BigNumber nInt, char* n, BigNumber xIn
 	for(unsigned short i = 254; i > 0; i -= 1) {
 		bit = (n[i/8] >> (i % 8)) & 0x01;
 
-//		Serial.print("bit: ");
-//		Serial.println(bit);
+		Serial.print("bit: ");
+		Serial.println(bit);
 
 		if(bit) {
-			ladderStep(X1, X3, Z3, X2, Z2);
-//			Serial.print("X1: ");
-//			Serial.println(X1);
+			ladderStep(X1, V);
+			Serial.print("X1: ");
+			Serial.println(X1);
 			Serial.print("V: ");
-			Serial.print(X3);
-			Serial.print(", ");
-			Serial.print(Z3);
-			Serial.print(", ");
-			Serial.print(X2);
-			Serial.print(", ");
-			Serial.println(Z2);
+			for(unsigned short j = 0; j < 4; j += 1) {
+				Serial.print(V[i]);
+				Serial.print(", ");
+				Serial.println();
+			}
 		} else {
-			ladderStep(X1, X2, Z2, X3, Z3);
-//			Serial.print("X1: ");
-//			Serial.println(X1);
+			ladderStep(X1, U);
+			Serial.print("X1: ");
+			Serial.println(X1);
 			Serial.print("U: ");
-			Serial.print(X2);
-			Serial.print(", ");
-			Serial.print(Z2);
-			Serial.print(", ");
-			Serial.print(X3);
-			Serial.print(", ");
-			Serial.println(Z3);
+			for(unsigned short j = 0; j < 4; j += 1) {
+				Serial.print(U[i]);
+				Serial.print(", ");
+				Serial.println();
+			}
 		}
 	}
 
@@ -242,11 +199,11 @@ BigNumber X25519KeyManagement::curve25519(BigNumber nInt, char* n, BigNumber xIn
 
 
 void X25519KeyManagement::createPubKey(char* n) { // k is the independent, uniform, random secret key. It is an array of 32 bytes and is used as the scalar for the elliptic curve.
-	BigNumber xInit = 9;
+	unsigned short xInit = 9;
 
 	n = transformScalar(n);
-	BigNumber nInt = makeInt(n, 32);
-	BigNumber x = curve25519(nInt, n, xInit);
+	nInt = makeInt(n, 32);
+	x = curve25519(nInt, n, xInit);
 	x = x % p;
 
 	Serial.println(x);
