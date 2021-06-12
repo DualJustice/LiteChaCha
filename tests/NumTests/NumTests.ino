00a0267e229b05159434b96643bcf8606ee215de
DualJustice <mattatgbsoftware@gmail.com>
2021-06-12 16:31:27 -0500

    Started thinking about 256 bit division
    Good luck.

--- tests/NumTests/NumTests.ino
+++ tests/NumTests/NumTests.ino
@@ -4,6 +4,14 @@
 #include "stdint.h"
 
 
+// fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0000000000000000000000000000000000000000000000000000000000000001 is the largest expected (HEX) value. (0xffffffff ...)^2
+
+// 1 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff fffffffe is the largest expected (HEX) sum.
+// 0 7fffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffed is p.
+// 0 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffda is p*2.
+// If A > (2*p), A = (A - (2*p)), else, A = (A - p).
+
+
 void setup() {
 	Serial.begin(9600);
 	while(!Serial) {
@@ -18,16 +26,72 @@ void setup() {
 	uint32_t temp;
 	char carry;
 
+	B[0] = 0x08080808; B[1] = 0x00000000; B[2] = 0x00000000; B[3] = 0xffffffff; B[4] = 0xffffffff; B[5] = 0x00000000; B[6] = 0x00000000; B[7] = 0xffffffff;
+	C[0] = 0x09090910; C[1] = 0x00000000; C[2] = 0x00000000; C[3] = 0x00000000; C[4] = 0xffffffff; C[5] = 0x00000000; C[6] = 0xffffffff; C[7] = 0xffffffff;
+
+	p[0] = 0x7fffffff; p[1] = 0xffffffff; p[2] = 0xffffffff; p[3] = 0xffffffff; p[4] = 0xffffffff; p[5] = 0xffffffff; p[6] = 0xffffffff; p[7] = 0xffffffed;
+
 	unsigned long timestamp = 0;
 
-//	A = (B + C) % p;
+/*
+// C++ implementation to Divide two
+// integers without using multiplication,
+// division and mod operator
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to divide a by b and
+// return floor value it
+int divide(long long dividend, long long divisor) {
+ 
+  // Calculate sign of divisor i.e.,
+  // sign will be negative only iff
+  // either one of them is negative
+  // otherwise it will be positive
+  int sign = ((dividend < 0) ^
+              (divisor < 0)) ? -1 : 1;
+ 
+  // remove sign of operands
+  dividend = abs(dividend);
+  divisor = abs(divisor);
+ 
+  // Initialize the quotient
+  long long quotient = 0, temp = 0;
+ 
+  // test down from the highest bit and
+  // accumulate the tentative value for
+  // valid bit
+  for (int i = 31; i >= 0; --i) {
+ 
+    if (temp + (divisor << i) <= dividend) {
+      temp += divisor << i;
+      quotient |= 1LL << i;
+    }
+  }
+ 
+  return sign * quotient;
+}
+ 
+// Driver code
+int main() {
+  int a = 10, b = 3;
+  cout << divide(a, b) << "\n";
+ 
+  a = 43, b = -8;
+  cout << divide(a, b);
+ 
+  return 0;
+}
+*/
+
+// -------------------- A = (B + C) % p -------------------- Can build mod directly into addition, or can create an external function. It is internal below.
 	timestamp = micros();
 	for(unsigned short t = 0; t < 500; t += 1) {
+		A[0] = 0x00000000; A[1] = 0x00000000; A[2] = 0x00000000; A[3] = 0x00000000; A[4] = 0x00000000; A[5] = 0x00000000; A[6] = 0x00000000; A[7] = 0x00000000;
+
+// -------------------- RELEVANT --------------------
+
 		carry = 0x00;
-
-		B[0] = 0x08080808; B[1] = 0x00000000; B[2] = 0x00000000; B[3] = 0xffffffff; B[4] = 0xffffffff; B[5] = 0x00000000; B[6] = 0x00000000; B[7] = 0xffffffff;
-		C[0] = 0x09090910; C[1] = 0x00000000; C[2] = 0x00000000; C[3] = 0x00000000; C[4] = 0xffffffff; C[5] = 0x00000000; C[6] = 0xffffffff; C[7] = 0xffffffff;
-
 
 		for(unsigned short i = 7; i < 8; i -= 1) {
 			temp = B[i];
@@ -39,7 +103,13 @@ void setup() {
 				carry = 0x00;
 			}
 		}
+
+		if((carry == 0x01) || (A[0] > p[0])) { // A > p.
+
+		}
 	}
+
+// -------------------- RELEVANT --------------------
 	timestamp = micros() - timestamp;
 
 	Serial.print("micros: ");
