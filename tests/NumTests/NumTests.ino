d4d0b35222da6c30062ddae7137df565e5a3e072
DualJustice <mattatgbsoftware@gmail.com>
2021-06-14 17:05:49 -0500

    Tried something.
    It is possible that polynomial division cannot be used in place of normal modulus... :|

--- tests/NumTests/NumTests.ino
+++ tests/NumTests/NumTests.ino
@@ -6,9 +6,38 @@
 
 // fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0000000000000000000000000000000000000000000000000000000000000001 is the largest expected (HEX) value. (0xffffffff ...)^2
 
-// 1 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff fffffffe is the largest expected (HEX) sum.
+// 1 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff fffffffe is the largest expected (HEX) sum (257 bits).
 // 0 7fffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffed is p (255 bits, because 7fffffff HEX is 01111111111111111111111111111111 BIN).
 // 0 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffda is p*2.
+
+// 80000000 = 10000000000000000000000000000000
+// 7fffffff = 01111111111111111111111111111111
+
+
+bool isGreaterThan(uint32_t* A, uint32_t* p) {
+	if(A[0] > p[0]) {
+		return true;
+	}
+	for(unsigned short i = 1; i < 7; i += 1) {
+		if(A[i] != p[i]) {
+			return false;
+		}
+	}
+	if(A[7] > p[7]) {
+		return true;
+	}
+	return false;
+}
+
+
+bool isEqualTo(uint32_t* A, uint32_t* p) {
+	for(unsigned short i = 0; i < 8; i += 1) {
+		if(A[i] != p[i]) {
+			return false;
+		}
+	}
+	return true;
+}
 
 
 void setup() {
@@ -26,8 +55,23 @@ void setup() {
 	uint32_t temp;
 	char carry;
 
-	B[0] = 0x08080808; B[1] = 0x00000000; B[2] = 0x00000000; B[3] = 0xffffffff; B[4] = 0xffffffff; B[5] = 0x00000000; B[6] = 0x00000000; B[7] = 0xffffffff;
-	C[0] = 0x09090910; C[1] = 0x00000000; C[2] = 0x00000000; C[3] = 0x00000000; C[4] = 0xffffffff; C[5] = 0x00000000; C[6] = 0xffffffff; C[7] = 0xffffffff;
+//	B[0] = 0x08080808; B[1] = 0x00000000; B[2] = 0x00000000; B[3] = 0xffffffff; B[4] = 0xffffffff; B[5] = 0x00000000; B[6] = 0x00000000; B[7] = 0xffffffff;
+//	C[0] = 0x09090910; C[1] = 0x00000000; C[2] = 0x00000000; C[3] = 0x00000000; C[4] = 0xffffffff; C[5] = 0x00000000; C[6] = 0xffffffff; C[7] = 0xffffffff;
+
+//	B[0] = 0x00000000; B[1] = 0x00000000; B[2] = 0x00000000; B[3] = 0x00000000; B[4] = 0x00000000; B[5] = 0x00000000; B[6] = 0x00000000; B[7] = 0x00000000;
+//	C[0] = 0x7fffffff; C[1] = 0xffffffff; C[2] = 0xffffffff; C[3] = 0xffffffff; C[4] = 0xffffffff; C[5] = 0xffffffff; C[6] = 0xffffffff; C[7] = 0xffffffed;
+
+//	B[0] = 0x00000000; B[1] = 0x00000000; B[2] = 0x00000000; B[3] = 0x00000000; B[4] = 0x00000000; B[5] = 0x00000000; B[6] = 0x00000000; B[7] = 0x00000001;
+//	C[0] = 0xffffffff; C[1] = 0xffffffff; C[2] = 0xffffffff; C[3] = 0xffffffff; C[4] = 0xffffffff; C[5] = 0xffffffff; C[6] = 0xffffffff; C[7] = 0xffffffda;
+
+	B[0] = 0x00000000; B[1] = 0x00000000; B[2] = 0x00000000; B[3] = 0x00000000; B[4] = 0x00000000; B[5] = 0x00000000; B[6] = 0x00000000; B[7] = 0x00000000;
+//       0   7fffffff           ffffffff           ffffffff           ffffffff           ffffffff           ffffffff           ffffffff           ffffffed is p
+	C[0] = 0x80000000; C[1] = 0x00000000; C[2] = 0x00000000; C[3] = 0x00000000; C[4] = 0x00000000; C[5] = 0x00000000; C[6] = 0x00000000; C[7] = 0x00000000;
+
+// 0xed = 11101101
+// 0x01 = 00000001
+
+// 0xee = 11101110
 
 	p[0] = 0x7fffffff; p[1] = 0xffffffff; p[2] = 0xffffffff; p[3] = 0xffffffff; p[4] = 0xffffffff; p[5] = 0xffffffff; p[6] = 0xffffffff; p[7] = 0xffffffed;
 
@@ -54,8 +98,62 @@ void setup() {
 			}
 		}
 
-		if((carry != 0x00) || isGreaterThan(A, p)) {
-			
+		if((carry != 0x00) || isGreaterThan(A, p)) { // Everything below assumes that p includes a leading 0! I don't think that polynomial division can be used in place of normal modulus.
+			// Do mod:
+			if(carry == 0x01) {
+				temp = (A[7] & 0x00000003);
+				for(unsigned short i = 7; i > 0; i -= 1) {
+					carry = A[i - 1] & 0x00000003; // This might not work?
+					A[i] = (A[i] >> 2) | (carry << 30);
+					A[i] ^= p[i];
+				}
+				A[0] = (A[0] >> 2) | 0x40000000;
+				A[0] ^= p[0];
+
+				for(unsigned short t = 0; t < 2; t += 1) {
+					for(unsigned short i = 0; i < 7; i += 1) {
+						carry = (A[i + 1] & 0x80000000) >> 31; // This might not work?
+						A[i] = (A[i] << 1) | carry; // This might not work?
+					}
+					carry = (temp >> (1 - t)) & 0x00000001; // This might not work?
+					A[7] = (A[7] << 1) | carry; // This might not work?
+
+					if((A[0] & 0x80000000) == 0x80000000) {
+						for(unsigned short i = 0; i < 8; i += 1) {
+							A[i] ^= p[i];
+						}
+					}
+				}
+			} else if((A[0] & 0x80000000) == 0x80000000) {
+				temp = (A[7] & 0x00000001);
+				for(unsigned short i = 7; i > 0; i -= 1) {
+					carry = A[i - 1] & 0x00000001; // This might not work?
+					A[i] = (A[i] >> 1) | (carry << 31);
+					A[i] ^= p[i];
+				}
+				A[0] = (A[0] >> 1);
+				A[0] ^= p[0];
+
+				for(unsigned short i = 0; i < 7; i += 1) {
+					carry = (A[i + 1] & 0x80000000) >> 31; // This might not work?
+					A[i] = (A[i] << 1) | carry; // This might not work?
+				}
+				A[7] = (A[7] << 1) | temp;
+
+				if((A[0] & 0x80000000) == 0x80000000) {
+					for(unsigned short i = 0; i < 8; i += 1) {
+						A[i] ^= p[i];
+					}
+				}
+			} else {
+				for(unsigned short i = 0; i < 8; i += 1) {
+					A[i] ^= p[i];
+				}
+			}
+		} else if(isEqualTo(A, p)) {
+			for(unsigned short i = 0; i < 8; i += 1) {
+				A[i] = 0x00000000;
+			}
 		}
 	}
 
