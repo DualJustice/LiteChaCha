adf1b7e96d52f7dc820da8213a5342eab175b0bd
DualJustice <mattatgbsoftware@gmail.com>
2021-06-20 17:45:21 -0500

    Reimagined addition
    Using 16 16-bit words as opposed to 8 32-bit words. Simplifies the carry condition and removes all conditional branches.

--- tests/NumTests/NumTests.ino
+++ tests/NumTests/NumTests.ino
@@ -25,12 +25,45 @@ two's complement of p (BIN):
 */
 
 
-uint32_t u[8];
-uint32_t v[8];
+uint32_t p[8]; // Might need to be 16 long?
+
+uint32_t a[8];
+uint32_t b[8];
+
+uint32_t u[16];
+uint32_t v[16];
 
 char carry;
 
-uint32_t p[8];
+
+void base32_16() {
+	for(unsigned short i = 0; i < 8; i += 1) {
+		u[i*2] = a[i] >> 16;
+		v[i*2] = b[i] >> 16;
+		u[(i*2) + 1] = a[i] & 0x0000ffff;
+		v[(i*2) + 1] = b[i] & 0x0000ffff;
+	}
+}
+
+
+// ---------- Addition Using: The Art Of Computer Programming, Vol. 2, Sec. 4.3.1, Algorithm A ----------
+void base16Add() { // Check for constant time. Might be able to optimize by combining some steps?
+	carry = 0x00;
+
+	for(unsigned short i = 15; i < 16; i -= 1) {
+		u[i] += (v[i] + carry);
+		carry = u[i]/0x00010000;
+		u[i] %= 0x00010000;
+	}
+}
+
+
+void base16_32() {
+	for(unsigned short i = 0; i < 8; i += 1) {
+		a[i] = (u[i*2] << 16) | u[(i*2) + 1];
+		b[i] = (v[i*2] << 16) | v[(i*2) + 1];
+	}
+}
 
 
 void setup() {
@@ -39,19 +72,28 @@ void setup() {
 		delay(250);
 	}
 
-	u[0] = 0x00000000; u[1] = 0x00000000; u[2] = 0x00000000; u[3] = 0x00000000; u[4] = 0x00000000; u[5] = 0x00000000; u[6] = 0x00000000; u[7] = 0x00000000;
-	v[0] = 0x00000000; v[1] = 0x00000000; v[2] = 0x00000000; v[3] = 0x00000000; v[4] = 0x00000000; v[5] = 0x00000000; v[6] = 0x00000000; v[7] = 0x00000000;
-
 	p[0] = 0x7fffffff; p[1] = 0xffffffff; p[2] = 0xffffffff; p[3] = 0xffffffff; p[4] = 0xffffffff; p[5] = 0xffffffff; p[6] = 0xffffffff; p[7] = 0xffffffed;
 
-// ---------- Addition Using: The Art Of Computer Programming, Vol. 2, Sec. 4.3.1, Algorithm A ----------
-	carry = 0x00;
+	a[0] = 0xffffffff; a[1] = 0xffffffff; a[2] = 0xffffffff; a[3] = 0xffffffff; a[4] = 0xffffffff; a[5] = 0xffffffff; a[6] = 0xffffffff; a[7] = 0xffffffff;
+	b[0] = 0xffffffff; b[1] = 0xffffffff; b[2] = 0xffffffff; b[3] = 0xffffffff; b[4] = 0xffffffff; b[5] = 0xffffffff; b[6] = 0xffffffff; b[7] = 0xffffffff;
 
-	for(unsigned short i = 7; i < 8; i -= 1) {
-		u[i] += (v[i] + carry);
-		carry = u[i]/v[i];
+//	u[0] = 0x00000000; u[1] = 0x00000000; u[2] = 0x00000000; u[3] = 0x00000000; u[4] = 0x00000000; u[5] = 0x00000000; u[6] = 0x00000000; u[7] = 0x00000000; u[8] = 0x00000000; u[9] = 0x00000000; u[10] = 0x00000000; u[11] = 0x00000000; u[12] = 0x00000000; u[13] = 0x00000000; u[14] = 0x00000000; u[15] = 0x00000000;
+//	v[0] = 0x00000000; v[1] = 0x00000000; v[2] = 0x00000000; v[3] = 0x00000000; v[4] = 0x00000000; v[5] = 0x00000000; v[6] = 0x00000000; v[7] = 0x00000000; v[8] = 0x00000000; v[9] = 0x00000000; v[10] = 0x00000000; v[11] = 0x00000000; v[12] = 0x00000000; v[13] = 0x00000000; v[14] = 0x00000000; v[15] = 0x00000000;
+
+	base32_16();
+	base16Add();
+	base16_32();
+
+
+	Serial.print(carry, HEX);
+	Serial.print(' ');
+	for(unsigned short i = 0; i < 8; i += 1) {
+		Serial.print(a[i], HEX);
+		Serial.print(' ');
 	}
+	Serial.println('\n');
 }
+
 /*
 void add() { // Will want to check all functions for constant time!
 	carry = 0x00;
