fcced8f9928ec0122b7894cf50e013c09ae9b3a1
DualJustice <mattatgbsoftware@gmail.com>
2021-06-21 12:19:38 -0500

    D3 complete?

--- tests/NumTests/NumTests.ino
+++ tests/NumTests/NumTests.ino
@@ -6,24 +6,29 @@
 
 /*
 0 7fffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffed is p (255 bits).
+0 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffda is p*2.
 
 1 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff fffffffe is the largest expected sum (257 bits).
 0 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff fffffffe 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000001 is the largest expected value (512 bits): (ffffffff ... )^2.
-
-0 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff ffffffda is p*2.
 */
 
-
-uint32_t p[16]; // These might need to be of a much larger size to account for the carry bit of addition, and the additional bits caused by multiplication.
 
 uint32_t a[8];
 uint32_t b[8];
 
-uint32_t u[18];
-uint32_t v[18];
+static const constexpr uint32_t p[16] = {0x00007fff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffed};
+static const constexpr uint32_t p2[16] = {0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffda};
+
+uint32_t u[18]; // u[0] is used for u[m + n], u[1] is used for carry.
+uint32_t v[17]; // v[0] is used for carry.
 
 char carry;
+static const constexpr uint32_t base = 0x00010000;
 
+// Modulus variables:
+static const constexpr char d = 0x02;
+static const constexpr unsigned short n = 16;
+unsigned short m;
 uint32_t qHat;
 uint32_t rHat;
 
@@ -32,37 +37,52 @@ unsigned long duration;
 
 
 void base32_16() {
+	u[1] = 0x00000000; // Coinsider moving.
+	v[0] = 0x00000000; // Coinsider moving.
 	for(unsigned short i = 0; i < 8; i += 1) {
-		u[0] = 0x00000000;
-		v[0] = 0x00000000;
-		u[1] = 0x00000000;
-		v[1] = 0x00000000;
 		u[(i*2) + 2] = a[i] >> 16;
-		v[(i*2) + 2] = b[i] >> 16;
 		u[(i*2) + 3] = a[i] & 0x0000ffff;
-		v[(i*2) + 3] = b[i] & 0x0000ffff;
+
+		v[(i*2) + 1] = b[i] >> 16;
+		v[(i*2) + 2] = b[i] & 0x0000ffff;
 	}
 }
 
 
 // ---------- Modulus Using: The Art Of Computer Programming, Vol. 2, Sec. 4.3.1, Algorithm D ----------
-void addMod() {
-	for(unsigned short i = 0; i < 18; i += 1) { // D1
-		v[i] = u[i];
+void base16Mod() {
+	for(unsigned short i = 1; i < (n + m + 1); i += 1) { // D1.
+		v[i - 1] = u[i];
 	}
-	base16Add();
+	carry = 0x00;
+	for(unsigned short i = 17; i > 0; i -= 1) {
+		u[i] += (v[i - 1] + carry);
+		carry = u[i]/base;
+		u[i] %= base;
+	}
+	u[0] = 0x00000000;
 
-	for(unsigned short i = 0; i < 2; i += 1) { // D2 { D3 - D7 }
-		qHat = ((2*u[i]) + u[i + 1])/0x0000ffff;
-		rHat = ((2*u[i]) + u[i + 1]) % 0x0000ffff;
+	for(unsigned short i = 0; i < (m + 1); i += 1) { // D2.
 
-		if((qHat == 0x00000002) || ((qHat*v[1]) > (2*rHat + u[2]))) {
-			qHat -= 1;
-			rHat += 0x0000ffff;
-			while(rHat < 0x00010000) {
-
-			} // Uh... come back to this.
+		qHat = ((base*u[i]) + u[i + 1])/p2[0]; // D3.
+		rHat = ((base*u[i]) + u[i + 1]) % p2[0];
+		if((qHat == base) || ((qHat*p2[1]) > ((base*rHat) + u[i + 2]))) {
+			qHat -= 0x01;
+			rHat += p2[0];
+			if(rHat < base) {
+				if((qHat == base) || ((qHat*p2[1]) > ((base*rHat) + u[i + 2]))) {
+					qHat -= 0x01;
+					rHat += p2[0];
+					if(rHat < base) {
+						Serial.print("ERROR!");
+						Serial.println('\n');
+						abort();
+					}
+				}
+			}
 		}
+
+	
 	}
 }
 
@@ -71,13 +91,14 @@ void addMod() {
 void base16Add() { // Check for constant time. Might be able to optimize by combining some steps? Addition could be done with 9 31-bit numbers, which would be an optimization, but it would require extra conversions for multiplication. It might be worth an attempt in the future.
 	carry = 0x00;
 
-	for(unsigned short i = 17; i > 1; i -= 1) {
-		u[i] += (v[i] + carry);
-		carry = u[i]/0x00010000;
-		u[i] %= 0x00010000;
+	for(unsigned short i = 17; i > 0; i -= 1) {
+		u[i] += (v[i - 1] + carry);
+		carry = u[i]/base;
+		u[i] %= base;
 	}
 
-	u[1] += carry;
+	m = 1;
+//	base16Mod();
 }
 
 
@@ -93,8 +114,6 @@ void setup() {
 	while(!Serial) {
 		delay(250);
 	}
-
-	p[0] = 0x7fffffff; p[1] = 0xffffffff; p[2] = 0xffffffff; p[3] = 0xffffffff; p[4] = 0xffffffff; p[5] = 0xffffffff; p[6] = 0xffffffff; p[7] = 0xffffffed;
 
 	duration = 0;
 	for(unsigned short t = 0; t < 500; t += 1) {
