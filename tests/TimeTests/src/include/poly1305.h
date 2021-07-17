27c012df8273da88bb24f893a8b0a9a74caaebcd
DualJustice <mattatgbsoftware@gmail.com>
2021-07-17 13:04:10 -0500

    Started fleshing out Poly1305
    Thinking through the problem, adding variable and function names.

--- tests/TimeTests/src/include/poly1305.h
+++ tests/TimeTests/src/include/poly1305.h
@@ -1,7 +1,6 @@
 #ifndef POLY1305_H
 #define POLY1305_H
 
-#include "chacha.h"
 #include "multiprecision1305.h"
 
 #include <stdint.h>
@@ -11,7 +10,7 @@
 - 256-bit one-time key, s, created using chacha.
 - the message.
 	- The message will be broken up into 16-byte blocks. Example block: 0001 6f46 2063 6968 7061 7267 6f74 7079 7243 (9, 16-bit words).
-	- The contents of the blocks will be read on little-endian.
+	- The contents of the blocks will be read in little-endian.
 - r (128-bit number).
 - s (128-bit number).
 - p =      0003 ffff ffff ffff ffff ffff ffff ffff fffb = (2^130) - 5 (9, 16-bit words).
@@ -26,10 +25,10 @@
 	2.2. The next 128 bits become "s."
 3. Encrypt the message using chacha with blockCounter set to 1.
 4. Initialize a to 0.
-5. Divide the block into 16-byte blocks. The last block might be shorter.
+5. Divide the message into 16-byte blocks. The last block might be shorter.
 LOOP THROUGH BLOCKS {
 	6. Read the block in little-endian order.
-	7. Add one bit beyond the number of octets. For the shorter block, it can be 2^120, 2^112, or any power of two that is evenly divisible by 8, all the way down to 2^8. (0x01, 0x.. ..)
+	7. Add one bit beyond the number of octets. For the shorter block, it can be 2^120, 2^112, or any power of two that is evenly divisible by 8, all the way down to 2^8. (0x01, 0x.., ...)
 	8. If the last block is not 17 bytes long (16 + 1 bit), pad (prepend) it with zeros. This is meaningless if you treat the blocks as numbers.
 	9. Add this number to the accumulator: a.
 	10. Multiply a by "r" and take the result mod p. { a = ((a + block)*r) % p }.
@@ -40,13 +39,25 @@ LOOP THROUGH BLOCKS {
 
 class Poly1305MAC {
 private:
-	ChaChaEncryption keygen;
 	MultiPrecisionArithmetic1305 math;
 
-	
+	static const constexpr unsigned short KEYLEN = 8;
+	static const constexpr unsigned short INTLEN = 4;
+	static const constexpr unsigned short INTLENMULTI = (2*INTLEN) + 1;
+
+	uint32_t r[INTLEN];
+	uint32_t s[INTLEN];
+
+	uint32_t rMulti[INTLENMULTI];
+	uint32_t sMulti[INTLENMULTI];
+
+	uint32_t a[INTLENMULTI];
 public:
 	Poly1305MAC();
 	~Poly1305MAC();
+
+	void createTag(char*, uint32_t[KEYLEN], char*, unsigned long long);
+	bool authenticate(uint32_t[KEYLEN], char*, unsigned long long, char*);
 };
 
 
@@ -56,6 +67,16 @@ Poly1305MAC::Poly1305MAC() {
 
 
 Poly1305MAC::~Poly1305MAC() {
+
+}
+
+
+void Poly1305MAC::createTag(char* out, uint32_t* key, char* message, unsigned long long bytes) {
+
+}
+
+
+bool Poly1305MAC::authenticate(uint32_t* key, char* message, unsigned long long bytes, char* tag) {
 
 }
 
