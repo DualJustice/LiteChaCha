b004b0b868d2795de339df1f92156ab6e40cae7c
DualJustice <mattatgbsoftware@gmail.com>
2021-07-30 11:54:46 -0500

    Added note.
    Paying some dues.

--- src/include/X25519.h
+++ src/include/X25519.h
@@ -53,7 +53,7 @@ private:
 	void cSwap(uint32_t);
 	void ladderStep();
 	void montgomeryLadder();
-	void reciprocal();
+	void inverse();
 
 	char* encodeXCoord(char*);
 	void checkAllZerosCase(char*);
@@ -187,7 +187,7 @@ void X25519KeyExchange::montgomeryLadder() {
 }
 
 
-void X25519KeyExchange::reciprocal() {
+void X25519KeyExchange::inverse() { // Copied directly from Daniel J. Bernstein. 
 	math.base16Mul(A, Z2, Z2);
 	math.base16Mul(Z3, A, A);
 	math.base16Mul(X3, Z3, Z3);
@@ -302,7 +302,7 @@ void X25519KeyExchange::curve25519(char* n, char* xp) { // n is the independent,
 
 	montgomeryLadder();
 
-	reciprocal();
+	inverse();
 	math.base16Mul(xIntMulti, X2, Z2);
 	math.base16_32(xInt, xIntMulti);
 
