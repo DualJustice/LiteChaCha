384bde13d8f83b61e1d39d7e9c839bfab7ef86d6
DualJustice <mattatgbsoftware@gmail.com>
2021-07-31 19:01:31 -0500

    Updated rng.h
    I wasn't satisfied with my implementation of RNG, so I made it better. This is definitely better than it was, but it may still not be satisfactory for anything truly important.

--- tests/TimeTests/src/include/rng.h
+++ tests/TimeTests/src/include/rng.h
@@ -9,9 +9,7 @@ private:
 	const unsigned short ANALOG_RESOLUTION = 12;
 	const unsigned short ANALOG_PIN = A0; // This analog pin must be floating!
 
-	unsigned long analogVal;
-	unsigned long previousAnalogVal;
-	unsigned long timeVal;
+	char bit;
 	unsigned long seedVal;
 
 	const unsigned short base = 0x100;
@@ -35,28 +33,15 @@ RNGen::~RNGen() {
 }
 
 
-void RNGen::initializeSeed() { // Truly, this is shameful. Please do better than me.
+void RNGen::initializeSeed() { // This should be handled as a bare minimum implementation of RNG. I am sure you can do better than me.
 	analogReadResolution(ANALOG_RESOLUTION);
 
-	seedVal = micros();
-	previousAnalogVal = 1;
+	seedVal = 0;
 
-	for(unsigned short j = 0; j < 10; j += 1) {
-		timeVal = micros();
+	for(unsigned short i = 0; i < 32; i += 1) {
+		bit = analogRead(ANALOG_PIN) & 0x00000001;
 
-		for(unsigned short i = 0; i < 10; i += 1) {
-			analogVal = analogRead(ANALOG_PIN) + 1;
-			seedVal += (analogVal + (analogVal/previousAnalogVal));
-			previousAnalogVal = analogVal;
-
-			seedVal ^= analogRead(ANALOG_PIN);
-			seedVal = (seedVal << 7) | (seedVal >> 25);
-		}
-
-		seedVal = (seedVal << 5) | (seedVal >> 27);
-
-		timeVal = (micros() + 1) - timeVal;
-		seedVal *= timeVal;
+		seedVal = (seedVal << 1) | bit;
 	}
 
 	srand(seedVal);
