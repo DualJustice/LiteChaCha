cee16d9583a5be3b04ae67e8b413d58697a88ea4
DualJustice <mattatgbsoftware@gmail.com>
2021-08-11 14:49:56 -0500

    First steps
    Started the first couple of steps of Ed25519.

--- tests/Ed25519Tests/src/include/Ed25519.h
+++ tests/Ed25519Tests/src/include/Ed25519.h
@@ -12,58 +12,24 @@ private:
 	SHA512Hash hash;
 	MultiPrecisionArithmetic25519 math;
 
-	static const constexpr unsigned short BYTE_LENGTH = 32;
-	static const constexpr unsigned short INT_LENGTH = 8;
-	static const constexpr unsigned short INT_LENGTH_MULTI = 2*INT_LENGTH;
-	static const constexpr unsigned short BITS = 255;
+	static const constexpr unsigned short HASH_BYTES = 64;
+	static const constexpr unsigned short KEY_BYTES = 32;
 
-	uint32_t nInt[INT_LENGTH];
-	uint32_t xInt[INT_LENGTH];
-	uint32_t xIntMulti[INT_LENGTH_MULTI];
+	char h[HASH_BYTES]; // Hash buffer.
 
-	uint32_t X1[INT_LENGTH_MULTI];
-	uint32_t X2[INT_LENGTH_MULTI];
-	uint32_t Z2[INT_LENGTH_MULTI];
-	uint32_t X3[INT_LENGTH_MULTI];
-	uint32_t Z3[INT_LENGTH_MULTI];
+	char s[KEY_BYTES]; // Secret scalar.
+	char prefix[KEY_BYTES];
+	char publicKey[KEY_BYTES];
 
-	uint32_t s;
-	uint32_t bit;
-	uint32_t mask;
-	uint32_t swap;
-
-	uint32_t A[INT_LENGTH_MULTI];
-	uint32_t AA[INT_LENGTH_MULTI];
-	uint32_t B[INT_LENGTH_MULTI];
-	uint32_t BB[INT_LENGTH_MULTI];
-	uint32_t E[INT_LENGTH_MULTI];
-	uint32_t C[INT_LENGTH_MULTI];
-	uint32_t D[INT_LENGTH_MULTI];
-	uint32_t DA[INT_LENGTH_MULTI];
-	uint32_t CB[INT_LENGTH_MULTI];
-
-	uint32_t A24[INT_LENGTH] = {0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0001db41}; // (486662 - 2)/4.
-	uint32_t A24Multi[INT_LENGTH_MULTI];
-
-	char zerosCheck;
-
-	char* decodeBytesLittleEndian(char*);
-	char* clampAndDecodeScalar(char*);
-	void toUInt(uint32_t*, char*);
-	char* maskAndDecodeXCoord(char*);
-
-	void cSwap(uint32_t);
-	void ladderStep();
-	void montgomeryLadder();
-	void inverse();
-
-	char* encodeXCoord(char*);
-	void checkAllZerosCase(char*);
+	void pruneHashBuffer();
 public:
 	Ed25519SignatureAlgorithm();
 	~Ed25519SignatureAlgorithm();
 
-	void curve25519(char[BYTE_LENGTH], char[BYTE_LENGTH]);
+	void initialize(char[KEY_BYTES]);
+
+	void sign();
+	void verify();
 };
 
 
@@ -77,6 +43,35 @@ Ed25519SignatureAlgorithm::~Ed25519SignatureAlgorithm() {
 }
 
 
+void Ed25519SignatureAlgorithm::pruneHashBuffer() {
+	h[0] &= 0xf8;
+	h[31] &= 0x7f;
+	h[31] |= 0x40;
+}
 
+
+void Ed25519SignatureAlgorithm::initialize(char* privateKey) {
+	hash.hashBytes(h, privateKey, KEY_BYTES);
+	pruneHashBuffer();
+
+	for(unsigned short i = 0; i < KEY_BYTES; i += 1) {
+		s[i] = h[(KEY_BYTES - 1) - i];
+	}
+	for(unsigned short i = KEY_BYTES; i < HASH_BYTES; i += 1) {
+		prefix[i - KEY_BYTES] = h[i];
+	}
+
+	
+}
+
+
+void Ed25519SignatureAlgorithm::sign() {
+
+}
+
+
+void Ed25519SignatureAlgorithm::verify() {
+
+}
 
 #endif
