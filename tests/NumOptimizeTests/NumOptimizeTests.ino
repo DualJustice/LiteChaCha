7740ccc398ffa0df2d5d3fb039193fe0e0a60204
DualJustice <mattatgbsoftware@gmail.com>
2021-10-07 07:35:32 -0500

    Started validating optimized Ed.
    Test vector 3 complete and passed. Might want to check out Blue.

--- tests/NumOptimizeTests/NumOptimizeTests.ino
+++ tests/NumOptimizeTests/NumOptimizeTests.ino
@@ -22,13 +22,16 @@ TO DO:
 - Get run-times for ecdhe, john, and mac. [DONE]
 - Get run-times for optimized versions of above objects. [DONE]
 - Validate optimized versions. []
+	- Ed25519 []
+	- X25519 []
+	- poly []
 
 */
 
 
 /*
----------- RESULTS: ----------
-	UNOPTIMIZED:
+---------- RUN-TIME RESULTS: ----------
+	UNOPTIMIZED TEST VECTOR 1:
 X25519 run-time: 2148959
 xp: C3DA55379DE9C6908E94EA4DF28D84F32ECCF3491C71F754B475577A28552
 
@@ -44,7 +47,7 @@ poly1305 authenticate run-time: 1626
 Authentic? 1
 
 
-	OPTIMIZED:
+	OPTIMIZED TEST VECTOR 1:
 X25519 run-time: 1439470
 xp: C3DA55379DE9C6908E94EA4DF28D84F32ECCF3491C71F754B475577A28552
 
@@ -72,6 +75,30 @@ optipolyAuth = (0.74)polyAuth [This is a maximum value, and will only decrease w
 */
 
 
+void stringToHex(char* out, char* s, unsigned short length = 32) { // Used for the Ed25519 Test Vectors.
+	for(unsigned short i = 0; i < length; i += 1) {
+		if(48 <= s[i*2] && s[i*2] <= 57) {
+			s[i*2] -= 48;
+		} else if(65 <= s[i*2] && s[i*2] <= 70) {
+			s[i*2] -= 55;
+		} else {
+			s[i*2] -= 87;
+		}
+		out[i] = s[i*2];
+		out[i] <<= 4;
+
+		if(48 <= s[(i*2) + 1] && s[(i*2) + 1] <= 57) {
+			s[(i*2) + 1] -= 48;
+		} else if(65 <= s[(i*2) + 1] && s[(i*2) + 1] <= 70) {
+			s[(i*2) + 1] -= 55;
+		} else {
+			s[(i*2) + 1] -= 87;
+		}
+		out[i] |= s[(i*2) + 1];
+	}
+}
+
+
 void setup() {
 	Serial.begin(9600);
 	while(!Serial) {
@@ -85,7 +112,7 @@ void setup() {
 	unsigned long timestamp = 0;
 	unsigned long duration = 0;
 
-
+/*
 //	X25519 Test Vector 1:
 	char n[32] = {0xa5, 0x46, 0xe3, 0x6b, 0xf0, 0x52, 0x7c, 0x9d, 0x3b, 0x16, 0x15, 0x4b, 0x82, 0x46, 0x5e, 0xdd, 0x62, 0x14, 0x4c, 0x0a, 0xc1, 0xfc, 0x5a, 0x18, 0x50, 0x6a, 0x22, 0x44, 0xba, 0x44, 0x9a, 0xc4};
 	char xp[32] = {0xe6, 0xdb, 0x68, 0x67, 0x58, 0x30, 0x30, 0xdb, 0x35, 0x94, 0xc1, 0xa4, 0x24, 0xb1, 0x5f, 0x7c, 0x72, 0x66, 0x24, 0xec, 0x26, 0xb3, 0x35, 0x3b, 0x10, 0xa9, 0x03, 0xa6, 0xd0, 0xab, 0x1c, 0x4c};
@@ -101,20 +128,20 @@ void setup() {
 		Serial.print(xp[i], HEX);
 	}
 	Serial.println('\n');
+*/
 
-
-//	Ed25519 Test Vector 1:
+//	Ed25519 Test Vectors:
 	char hancock[64];
 	char EdPubKey[32];
-	char EdPrivKey[32] = {0x9d, 0x61, 0xb1, 0x9d, 0xef, 0xfd, 0x5a, 0x60, 0xba, 0x84, 0x4a, 0xf4, 0x92, 0xec, 0x2c, 0xc4, 0x44, 0x49, 0xc5, 0x69, 0x7b, 0x32, 0x69, 0x19, 0x70, 0x3b, 0xac, 0x03, 0x1c, 0xae, 0x7f, 0x60};
-	unsigned short EdMessageLength = 0;
-	char EdMessageBuffer[EdMessageLength + 1] = {'\n'};
+	char EdPrivKeyBuffer[65] = {"c5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7"};
+	char EdPrivKey[32];
+	unsigned short EdMessageLength = 2;
+	char EdMessageBuffer[(EdMessageLength*2) + 1] = {"af82"};
 	char EdMessage[EdMessageLength];
 	bool EdValid;
 
-	for(unsigned short i = 0; i < EdMessageLength; i += 1) {
-		EdMessage[i] = EdMessageBuffer[i];
-	}
+	stringToHex(EdPrivKey, EdPrivKeyBuffer);
+	stringToHex(EdMessage, EdMessageBuffer, EdMessageLength);
 
 	timestamp = micros();
 		john.sign(hancock, EdPubKey, EdPrivKey, EdMessage, true, EdMessageLength);
@@ -124,12 +151,22 @@ void setup() {
 	Serial.println(duration);
 	Serial.print("Public Key: ");
 	for(unsigned short i = 0; i < 32; i += 1) {
-		Serial.print(EdPubKey[i], HEX);
+		if(EdPubKey[i] > 0x0f) {
+			Serial.print(EdPubKey[i], HEX);
+		} else {
+			Serial.print('0');
+			Serial.print(EdPubKey[i], HEX);
+		}
 	}
 	Serial.println();
 	Serial.print("Signature: ");
 	for(unsigned short i = 0; i < 64; i += 1) {
-		Serial.print(hancock[i], HEX);
+		if(hancock[i] > 0x0f) {
+			Serial.print(hancock[i], HEX);
+		} else {
+			Serial.print('0');
+			Serial.print(hancock[i], HEX);
+		}
 	}
 	Serial.println();
 
@@ -141,9 +178,15 @@ void setup() {
 	Serial.println(duration);
 	Serial.print("Valid? ");
 	Serial.println(EdValid);
+
+	hancock[63] += 0x01;
+	EdValid = john.verify(EdPubKey, EdMessage, hancock, EdMessageLength);
+
+	Serial.print("Invalid? ");
+	Serial.println(EdValid);
 	Serial.println();
 
-
+/*
 //	poly1305 Test Vector 1:
 	char tag[16];
 	uint32_t polyKey[8] = {0x85d6be78, 0x57556d33, 0x7f4452fe, 0x42d506a8, 0x0103808a, 0xfb0db2fd, 0x4abff6af, 0x4149f51b};
@@ -177,6 +220,7 @@ void setup() {
 	Serial.print("Authentic? ");
 	Serial.println(polyValid);
 	Serial.println();
+*/
 }
 
 
