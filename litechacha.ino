29d5d165eaaab55e4c56e20a5ca67da52f5977e7
DualJustice <mattatgbsoftware@gmail.com>
2022-02-13 11:28:10 -0600

    Changed char to unsigned char
    unsigned char is more explicit. Conceptually LiteChaCha works with bytes, not with characters.

--- litechacha.ino
+++ litechacha.ino
@@ -18,25 +18,25 @@ void setup() {
 	const size_t signatureBytes = pki.getSignatureBytes();
 	const size_t IDBytes = pki.getIDBytes();
 
-	char userDSAPrivateKey[keyBytes]; // Used as either an input or an output depending on whether the user would like to generate a new key pair.
-	char userDSAPubKey[keyBytes]; // Used as either an input or an output depending on whether the user would like to generate a new key pair.
-	char peerDSAPubKey[keyBytes];
+	unsigned char userDSAPrivateKey[keyBytes]; // Used as either an input or an output depending on whether the user would like to generate a new key pair.
+	unsigned char userDSAPubKey[keyBytes]; // Used as either an input or an output depending on whether the user would like to generate a new key pair.
+	unsigned char peerDSAPubKey[keyBytes];
 	bool generateNewDSAKeys = true;
 
-	char userEphemeralPubKey[keyBytes];
-	char peerEphemeralPubKey[keyBytes];
+	unsigned char userEphemeralPubKey[keyBytes];
+	unsigned char peerEphemeralPubKey[keyBytes];
 
-	char userSignature[signatureBytes];
-	char peerSignature[signatureBytes];
+	unsigned char userSignature[signatureBytes];
+	unsigned char peerSignature[signatureBytes];
 
-	char userID[IDBytes]; // IDs are used for a fixed portion of a nonce.
-	char peerID[IDBytes];
+	unsigned char userID[IDBytes]; // IDs are used for a fixed portion of a nonce.
+	unsigned char peerID[IDBytes];
 
 	const size_t tagBytes = ae.getTagBytes();
 
 	unsigned long long messageCount; // Used to increment a nonce for each new message sent. Will be sent with each encrypted message.
 
-	char tag[tagBytes]; // Used to authenticate encrypted messages. Will be sent with each encrypted message.
+	unsigned char tag[tagBytes]; // Used to authenticate encrypted messages. Will be sent with each encrypted message.
 
 
 // -------------------- Establish Connection --------------------
@@ -65,13 +65,13 @@ void setup() {
 		message[i] = messageBuffer[i];
 	}
 
-	ae.encryptAndTagMessage(messageCount, tag, message, messageBytes); // Encrypts message, overwriting it with the ciphertext. Outputs messageCount and the MAC tag as well.
+	ae.encryptAndTagMessage(messageCount, tag, (unsigned char*)message, messageBytes); // Encrypts message, overwriting it with the ciphertext. Outputs messageCount and the MAC tag as well.
 
 //	Send the messageCount, tag, and message.
 
 //	Upon receiving a ciphertext, messageCount, and tag:
-	if(ae.messageAuthentic(message, messageBytes, messageCount, tag)) { // Authenticates the message with the MAC tag.
-		ae.decryptAuthenticatedMessage(message, messageBytes, messageCount); // Decrypts message, overwriting it with the plaintext.
+	if(ae.messageAuthentic((unsigned char*)message, messageBytes, messageCount, tag)) { // Authenticates the message with the MAC tag.
+		ae.decryptAuthenticatedMessage((unsigned char*)message, messageBytes, messageCount); // Decrypts message, overwriting it with the plaintext.
 	}
 }
 
